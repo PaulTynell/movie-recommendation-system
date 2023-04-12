7be1fa8bef5b017e172080c45b7b25f70214b7e2
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2023-04-12 22:47:58 +0300

    get_trending function done

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 
 import requester
 
+def print_dict(dicti: dict):
+    for x, y in dicti.items():
+        print(x, y)
+
+
+
 def main():
 
     api = requester.Requester()
 
-    trending_tv = api.get_trending("tv", "week")
+    trending_tv = api.get_trending("movie", "week")
     print(trending_tv)
+    print_dict(trending_tv)
 
 
 if __name__ == '__main__':
--- requester.py
+++ requester.py
@@ -21,19 +21,28 @@ class Requester():
         self.response = f"https://api.themoviedb.org/3/trending/{self.media_type}/{self.time_window}?api_key={self.api_key}"
 
         trending = requests.get(self.response).json()
+        print(trending)
 
         if self.media_type == "tv":
-            trending_shows = []
+            trending_shows = dict()
             shows = trending['results']
             for show in shows:
-                tv_show = video.Tv_show(show['id'], "2023-05-01", show['name'])
-                trending_shows[show['name']] = tv_show
+                id = show['id']
+                release_date = show['first_air_date']
+                name = show['name']
+                tv_show = video.Tv_show(id, release_date, name)
+                trending_shows[name] = tv_show
             return trending_shows
 
-
         elif self.media_type == "movie":
+            trending_movies = dict()
             movies = trending['results']
             for movie in movies:
-                print(movie['title'])
-
+                id = movie['id']
+                release_date = movie['release_date']
+                name = movie['title']
+                movie_o = video.Movie(id, release_date, name)
+                trending_movies[name] = movie_o
+            return trending_movies
+    
     
